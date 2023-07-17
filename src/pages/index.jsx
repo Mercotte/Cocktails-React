8b401df20b3198e56ff6858bd9163128e801048c
Mercotte <sbhpro@outlook.fr>
2023-07-17 23:02:30 +0200

    Coktails-React

---
+++ src/pages/index.jsx
@@ -0,0 +1,19 @@
+import Home from '../pages/Home';
+import About from '../pages/About';
+import SharedLayout from '../pages/SharedLayout';
+import Error from '../pages/Error';
+import Cocktail from '../Components/Cocktail';
+import CocktailList from '../Components/CocktailList';
+import SingleCocktail from '../pages/SingleCoktails';
+import SearchForm from '../components/SearchForm.jsx';
+
+export {
+  Home,
+  About,
+  SharedLayout,
+  Error,
+  Cocktail,
+  CocktailList,
+  SingleCocktail,
+  SearchForm,
+};
